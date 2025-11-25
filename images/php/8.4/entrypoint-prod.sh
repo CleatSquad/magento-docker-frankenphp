545651a6a15f13e1a4a166fc0d5d29d75a1cd1db
Mohamed El Mrabet <mimou78@gmail.com>
2025-11-25 23:45:09 +0100

    Adding image info

--- images/php/8.4/entrypoint-prod.sh
+++ images/php/8.4/entrypoint-prod.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Fix Magento permissions for www-data
-chown -R www-data:www-data /var/www/html
+chmod +x bin/magento
 find /var/www/html -type d -exec chmod 755 {} \;
 find /var/www/html -type f -exec chmod 644 {} \;
 chmod -R 775 /var/www/html/var
